4f62e2b904c95a74c47ea925af1ff7819beeb4a8
Ben Gohlke <jcgohlke@me.com>
2015-03-11 12:34:56 -0400

    Added The Iron Yard 2 project to Week3/Apps. This covers Obj-C Categories.

---
+++ InClassResources/Week3/Apps/The Iron Yard-2/The Iron Yard/Car.h
@@ -0,0 +1,22 @@
+//
+//  Car.h
+//  The Iron Yard
+//
+//  Created by Ben Gohlke on 3/4/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+
+@property (copy) NSString *make;
+@property (copy) NSString *model;
+@property (copy) NSString *color;
+
+- (instancetype)initWithMake:(NSString *)make model:(NSString *)model andColor:(NSString *)color;
+
+// Performs drive action, and returns success or failure
+- (BOOL)drive;
+
+@end
---
+++ InClassResources/Week3/Apps/The Iron Yard-2/The Iron Yard/main.m
@@ -0,0 +1,31 @@
+//
+//  main.m
+//  The Iron Yard
+//
+//  Created by Ben Gohlke on 3/4/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Car+Extras.h"
+
+void driveCar(Car *car);
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
+        Car *aCar = [[Car alloc] initWithMake:@"Toyota"
+                                        model:@"Camry"
+                                     andColor:@"Red"];
+        
+        NSLog(@"%@ %@ is %@", aCar.make, aCar.model, aCar.color);
+        
+        [aCar drive];
+        
+        [aCar paint:@"Blue"];
+
+        NSLog(@"%@ %@ is %@", aCar.make, aCar.model, aCar.color);
+    }
+    return 0;
+}
