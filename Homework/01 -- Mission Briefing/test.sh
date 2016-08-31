2eee401e86a26a751cda82175907db138af4168b
Matt Dupree <kmdupr33@gmail.com>
2016-08-30 22:28:15 -0400

    Modify test script to use more generic simulator name

--- Homework/01 -- Mission Briefing/test.sh
+++ Homework/01 -- Mission Briefing/test.sh
@@ -1,1 +1,1 @@
-xcodebuild test -project Mission\ Briefing.xcodeproj/ -scheme Mission\ Briefing -destination 'id=585E9269-2E89-41AF-9902-2C68172F3185'
+xcodebuild test -project Mission\ Briefing.xcodeproj/ -scheme Mission\ Briefing -destination 'name=iphone 6s'
